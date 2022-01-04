171cb6c033c78d4733f9394535420ca03360f1e9
HoshinoSuzumi <boxlab@foxmail.com>
2022-01-04 22:01:29 +0800

    fetch data test

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+import pprint
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import artdaq
+from artdaq.constants import AcquisitionType
 
+pp = pprint.PrettyPrinter(indent=4)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+with artdaq.Task() as task:
+    task.ai_channels.add_ai_voltage_chan("Misaka/ai0")
+    task.timing.cfg_samp_clk_timing(500, sample_mode=AcquisitionType.CONTINUOUS, samps_per_chan=50)
+    for _ in range(1):
+        data = task.read(number_of_samples_per_channel=10)
+        print("Data:")
+        pp.pprint(data)
