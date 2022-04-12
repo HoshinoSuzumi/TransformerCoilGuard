b3cf8eb77c71dbb16514162e4e07b634dad9f882
HoshinoSuzumi <boxlab@foxmail.com>
2022-04-12 12:19:34 +0800

    fake data 分三组数据

--- main.py
+++ main.py
@@ -13,9 +13,17 @@ from artdaq.constants import AcquisitionType
 def acquire_ui(samples, rate=15000, fake_data=False):
     if fake_data:
         x = linspace(0, 1, rate)
-        u_sequence = 4 * sin(2 * pi * 50 * x)
-        i_sequence = 0.005 * sin(2 * pi * 50 * (x + random.normal(0, 0.5, rate)))
-        return u_sequence, i_sequence
+        a_u_sequence = 4 * sin(2 * pi * 50 * x)
+        a_i_sequence = 0.005 * sin(2 * pi * 50 * (x + random.normal(0, 0.5, rate)))
+        b_u_sequence = 4 * sin(2 * pi * 50 * x)
+        b_i_sequence = 0.005 * sin(2 * pi * 50 * (x + random.normal(0, 0.4, rate)))
+        c_u_sequence = 4 * sin(2 * pi * 50 * x)
+        c_i_sequence = 0.005 * sin(2 * pi * 50 * (x + random.normal(0, 0.6, rate)))
+        return {
+            'A': [a_u_sequence, a_i_sequence],
+            'B': [b_u_sequence, b_i_sequence],
+            'C': [c_u_sequence, c_i_sequence],
+        }
 
     with artdaq.Task() as task:
         task.ai_channels.add_ai_voltage_chan('Misaka/ai0')
@@ -23,9 +31,9 @@ def acquire_ui(samples, rate=15000, fake_data=False):
         task.timing.cfg_samp_clk_timing(rate, sample_mode=AcquisitionType.FINITE, samps_per_chan=1000)
 
         acquired_data = task.read(number_of_samples_per_channel=samples)
-        u_sequence = acquired_data[0]
-        i_sequence = acquired_data[1]
-        return u_sequence, i_sequence
+        a_u_sequence = acquired_data[0]
+        a_i_sequence = acquired_data[1]
+        return a_u_sequence, a_i_sequence
 
 
 def fft_ma_calculate(s, fs, n1, f1):
@@ -47,10 +55,6 @@ def lissajous_figure_calc(signal_u, signal_i, fs=1200, f1=50, n1=600):
 
 
 if __name__ == '__main__':
-    # x = linspace(0, 1, 15000)
-    # signal_u = 4 * sin(2 * pi * 50 * x)
-    # signal_i = 0.005 * sin(2 * pi * 50 * (x + random.normal(0, 0.5, 15000)))
-
     time_increment = 0
 
     azureClient = IoTHubDeviceClient.create_from_connection_string(
@@ -62,30 +66,26 @@ if __name__ == '__main__':
 
     while True:
         # 15000 data/s
-        U, I = acquire_ui(1000, rate=15000)
-        X, Y = lissajous_figure_calc(U, I, 15000, 50, 1000)
+        UA, IA = acquire_ui(1000, rate=15000, fake_data=False)['A']
+        UB, IB = acquire_ui(1000, rate=15000, fake_data=False)['B']
+        UC, IC = acquire_ui(1000, rate=15000, fake_data=False)['C']
+        XA, YA = lissajous_figure_calc(UA, IA, 15000, 50, 1000)
+        XB, YB = lissajous_figure_calc(UB, IB, 15000, 50, 1000)
+        XC, YC = lissajous_figure_calc(UC, IC, 15000, 50, 1000)
 
-        # data_dict = {
-        #     'lissajous': {
-        #         'X': X.tolist(),
-        #         'Y': Y.tolist()
-        #     },
-        #     'voltages': U,
-        #     'currents': I
-        # }
         data_dict = {
             'lissajous': {
                 "A": {
-                    "X": X.tolist(),
-                    "Y": Y.tolist()
+                    "X": XA.tolist(),
+                    "Y": YA.tolist()
                 },
                 "B": {
-                    "X": X.tolist(),
-                    "Y": Y.tolist()
+                    "X": XB.tolist(),
+                    "Y": YB.tolist()
                 },
                 "C": {
-                    "X": X.tolist(),
-                    "Y": Y.tolist()
+                    "X": XC.tolist(),
+                    "Y": YC.tolist()
                 }
             }
         }
@@ -98,20 +98,20 @@ if __name__ == '__main__':
 
         plt.subplot(221)
         plt.title('Sampling U & I')
-        plt.plot(U)
-        plt.plot(I)
+        plt.plot(UA)
+        plt.plot(IA)
 
         plt.subplot(222)
         plt.title('Lissajous figure')
-        plt.plot(X, Y)
+        plt.plot(XA, YA)
 
         plt.subplot(223)
         plt.title('Acquired data U')
-        plt.plot(U)
+        plt.plot(UA)
 
         plt.subplot(224)
         plt.title('Acquired data I')
-        plt.plot(I)
+        plt.plot(IA)
 
         plt.show()
 
